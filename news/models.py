ae6af94b5a650a9ad67458dc5034f1775277c4b4
Dmitry <167958570+slapsing@users.noreply.github.com>
2025-07-22 17:34:45 +0300

    Add files via upload

--- news/models.py
+++ news/models.py
@@ -8,9 +8,9 @@ class Author(models.Model):
         User,
         on_delete=models.CASCADE,
         primary_key=True,
-        related_name='author'  # Доступ через user.author
+        related_name='author_profile'  # Доступ через user.author
     )
-    rating = models.FloatField(default=0, blank=True)
+    rating = models.FloatField(default=0)
 
     def __str__(self):
         return self.user.username
@@ -20,7 +20,7 @@ class Author(models.Model):
         post_rating *= 3
         comment_rating = self.user.comments.aggregate(cr=Sum('rating'))['cr'] or 0
         post_comments_rating = Comment.objects.filter(post__author=self).aggregate(pcr=Sum('rating'))['pcr'] or 0
-
+        self.rating = post_rating + comment_rating + post_comments_rating
         self.save()
 
 
@@ -42,10 +42,11 @@ class Post(models.Model):
     author = models.ForeignKey('Author',
                                on_delete=models.CASCADE,
                                related_name='posts')  # связь «один ко многим» с моделью Author, доступ через author.posts
+    post_type = models.CharField(max_length=2, choices=POST_TYPES, default=ARTICLE)
     title = models.CharField(max_length=100)  # заголовок статьи/новости;
     content = models.TextField()  # текст статьи/новости;
     rating = models.FloatField(blank=True, null=True)  # рейтинг статьи/новости.
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # связь «многие ко многим» с моделью Category
+    category = models.ManyToManyField(Category, through='PostCategory')  # связь «многие ко многим» с моделью Category
     timestamp = models.DateTimeField(auto_now_add=True)  # автоматически добавляемая дата и время создания
 
     def __str__(self):
@@ -66,8 +67,8 @@ class Post(models.Model):
 class PostCategory(models.Model):
     """Промежуточная модель для связи «многие ко многим»:
     """
-    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='post_categories')
-    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='category_posts')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_categories')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category_posts')
 
     class Meta:
         unique_together = ('post', 'category')
@@ -80,9 +81,9 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE,
                              related_name='comments')  # связь «один ко многим» с моделью Post; доступ через post.comments
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')  # доступ через user.comments
-    comment = models.TextField()  # текст комментария;
+    text = models.TextField()  # текст комментария;
     timestamp = models.DateTimeField(auto_now_add=True)  # дата и время создания комментария;
-    rating = models.FloatField(blank=True, null=True)  # рейтинг комментария.
+    rating = models.FloatField(default=0)  # рейтинг комментария.
 
     def __str__(self):
         return f'Комментарий от {self.user.username}'
